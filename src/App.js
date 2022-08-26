f47780cdc11fab9d7e5691c026c957ad9da41dff
Spencer Renfro <spencer.renfro13@gmail.com>
2022-08-26 13:14:07 -0600

    added tailwind css, and components to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import UnitCard from './components/UnitCard';
+import './index.css'
+import { Button } from "@material-tailwind/react";
+ 
+
+
 
 function App() {
   return (
     <div className="App">
+    <Navbar />
+    <div className="flex flex-row mt-5 mx-10 justify-center">
+      <UnitCard />
+      <UnitCard />
+    </div>
+    <div className="flex flex-row justify-center">
+    <Button>Calculate Results</Button>
+    </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,6 +32,10 @@ function App() {
         >
           Learn React
         </a>
+
+        <h1 className="text-3xl font-bold underline">
+        Hello world!
+        </h1>
       </header>
     </div>
   );
