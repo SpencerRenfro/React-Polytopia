5017f82ef806e6af28825627cb08b1b115d22026
Spencer Renfro <spencer.renfro13@gmail.com>
2022-09-09 14:09:39 -0600

    added modal and pictures

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import UnitCard from './components/UnitCard';
-import './index.css'
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import UnitCard from "./components/UnitCard";
+import "./index.css";
 import { Button } from "@material-tailwind/react";
-import Results from './components/Results';
-import {useState} from 'react';
-
+import Results from "./components/Results";
+import { useState } from "react";
 
 function App() {
-
   const characters = {
     warrior: {
       name: "Warrior",
       maxHealth: 10,
       attack: 2,
       defence: 2,
-    
+      imageSource: "Warrior.png",
     },
     swordsman: {
       name: "Swordsman",
       maxHealth: 15,
       attack: 3,
       defence: 3,
+      imageSource: "Swordsman.png",
     },
     knight: {
-        name: "Knight",
-        maxHealth: 15,
-        attack: 3.5,
-        defence: 3,
+      name: "Knight",
+      maxHealth: 15,
+      attack: 3.5,
+      defence: 3,
+      imageSource: "Knight.png",
     },
     defender: {
       name: "Defender",
       maxHealth: 15,
       attack: 1,
       defence: 3,
-    }
-  }
-// ///////////////////
+      imageSource: "Defender.png",
+    },
+  };
+  // ///////////////////
   var battleResult = {
     attacker: {
       originalHp: 0,
@@ -50,8 +51,8 @@ function App() {
       damageDone: 0,
       healthRemaining: 0,
       survived: true,
-    }
-  }
+    },
+  };
 
   // ////////////
   var selectedAttacker = {
@@ -62,7 +63,8 @@ function App() {
     defence: 0,
     isVeteran: false,
     isBoosted: false,
-  }
+    imageSource: "Warrior.png",
+  };
   var selectedDefender = {
     name: "warrior",
     maxHealth: 0,
@@ -73,18 +75,38 @@ function App() {
     isBoosted: false,
     isPoisoned: false,
     isVeteran: false,
-  }
-
+    imageSource: "Warrior.png",
+  };
 
   const [charData, setCharData] = useState(characters);
+  const [selectedAttackerData, setSelectedAttackerData] =
+    useState(selectedAttacker);
+  const [selectedDefenderData, setSelectedDefenderData] =
+    useState(selectedDefender);
 
+  const [modalPopup, setModalPopup] = useState(false);  
+
+  function testFunction() {
+    console.log("FIRED");
+    
+  }
 
   return (
     <div className="App">
       <Navbar />
-      <div className="flex flex-row mt-5 mx-10 justify-center">
-        <UnitCard charData={charData} isAttacker={true}/>
-        <UnitCard charData={charData} isAttacker={false}/>
+      <div className="flex flex-row mt-5 mx-10 justify-center gap-6">
+        <UnitCard
+          selectedUnitData={selectedAttackerData}
+          isAttacker={true}
+          setModalPopup={setModalPopup}
+          modalPopup={modalPopup}
+        />
+        <UnitCard
+          selectedUnitData={selectedDefenderData}
+          isAttacker={false}
+          setModalPopup={setModalPopup}
+          modalPopup={modalPopup}
+        />
         {/* {UnitCard({charData,isAttacker:false})} */}
       </div>
       <div className="flex flex-row justify-center">
@@ -94,6 +116,24 @@ function App() {
         <Results />
       </div>
 
+      <label htmlFor="my-modal" className="btn modal-button">open modal</label>
+    <input type="checkbox" checked={modalPopup} id="my-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">
+            Congratulations random Internet user!
+          </h3>
+          <p className="py-4">
+            You've been selected for a chance to get one year of subscription to
+            use Wikipedia for free!
+          </p>
+          <div className="modal-action">
+            <label htmlFor="my-modal" className="btn">
+            </label>
+            <button class="btn btn-primary" onClick={() => setModalPopup(!modalPopup)}>Select Unit</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/UnitCard.js
+++ src/components/UnitCard.js
@@ -1,22 +1,101 @@
+export default function UnitCard(props) {
+  console.log(props);
 
-   
-  export default function UnitCard(props) {
-console.log(props);
-    
+  let imageSource = `${process.env.PUBLIC_URL}/Defender-images/${props.selectedUnitData.imageSource}`;
+  if (props.isAttacker) {
+    imageSource = `${process.env.PUBLIC_URL}/Attacker-images/${props.selectedUnitData.imageSource}`;
+  }
 
-    return (
-      <div class="card w-96 bg-base-100 shadow-xl">
-    
+  let cardTitle = "Defender";
+  if (props.isAttacker) {
+    cardTitle = "Attacker";
+  }
+
+  return (
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <div className="p-3">
+        <h2 class="card-title">{cardTitle}</h2>
+      </div>
+
       <figure class="px-10 pt-10">
-        <img src="https://placeimg.com/400/225/arch" alt="Shoes" class="rounded-xl" />
+        <img src={imageSource} width={150} alt="Shoes" class="rounded-xl" />
       </figure>
-      <div class="card-body items-center text-center">
-        <h2 class="card-title">{props.isAttacker.toString()}</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions">
-          <button class="btn btn-primary">Buy Now</button>
+
+      <div class="card-body items-left text-center flex flex-col justify-between">
+        <div className="unitcard-inputs">
+          <input
+            type="number"
+            placeholder="Enter Input Health"
+            className="input input-bordered input-primary w-full max-w-xs"
+          />
+
+          <div className="form-control">
+            <label className="label cursor-pointer">
+              <span className="label-text">Is veteran?</span>
+              <input
+                type="checkbox"
+                checked={props.isVeteran}
+                className="checkbox checkbox-primary"
+              />
+            </label>
+          </div>
+          {props.isAttacker && (
+            <div className="form-control">
+              <label className="label cursor-pointer">
+                <span className="label-text">Is it boosted?</span>
+                <input
+                  type="checkbox"
+                  checked={props.isBoosted}
+                  className="checkbox checkbox-primary"
+                />
+              </label>
+            </div>
+          )}
+
+          {!props.isAttacker && (
+            <div className="form-control">
+              <label className="label cursor-pointer">
+                <span className="label-text">Is it poisioned?</span>
+                <input
+                  type="checkbox"
+                  checked={props.isBoosted}
+                  className="checkbox checkbox-primary"
+                />
+              </label>
+            </div>
+          )}
+          {!props.isAttacker && (
+            <div className="form-control">
+              <label className="label cursor-pointer">
+                <span className="label-text">Defense bonus?</span>
+                <input
+                  type="checkbox"
+                  checked={props.isBoosted}
+                  className="checkbox checkbox-primary"
+                />
+              </label>
+            </div>
+          )}
+          {!props.isAttacker && (
+            <div className="form-control">
+              <label className="label cursor-pointer">
+                <span className="label-text">City wall?</span>
+                <input
+                  type="checkbox"
+                  checked={props.isBoosted}
+                  className="checkbox checkbox-primary"
+                />
+              </label>
+            </div>
+          )}
+        </div>
+        <div className="unitcard-button">
+          <div class="card-actions">
+            <button class="btn btn-primary" onClick={() => props.setModalPopup(!props.modalPopup)}>Select Unit</button>
+            <label htmlFor="my-modal" className="btn modal-button">open modal</label>
+          </div>
         </div>
       </div>
-    </div> 
-    );
-  }
+    </div>
+  );
+}
